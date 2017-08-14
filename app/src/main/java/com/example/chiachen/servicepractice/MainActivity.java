b82528a489f9f62d1d54fab4ad781092c7608c05
ChiaChen <s124202468@gmail.com>
2017-08-14 12:51:43 +0800

    listener

--- app/src/main/java/com/example/chiachen/servicepractice/MainActivity.java
+++ app/src/main/java/com/example/chiachen/servicepractice/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.chiachen.servicepractice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-	
+	View view;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		view = findViewById(R.id.Test);
+		view.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Log.e(MainActivity.class.getSimpleName(), "Test");
+			}
+		});
 	}
 }
