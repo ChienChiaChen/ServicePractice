b9e412139f962be3f51186ba6df135e15260818e
ChiaChen <s124202468@gmail.com>
2017-08-14 13:06:09 +0800

    Service never die

--- app/src/main/java/com/example/chiachen/servicepractice/MainActivity.java
+++ app/src/main/java/com/example/chiachen/servicepractice/MainActivity.java
@@ -1,16 +1,32 @@
 package com.example.chiachen.servicepractice;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+	Intent mServiceIntent;
+	private SensorService mSensorService;
 	View view;
+	Context ctx;
+	public Context getCtx() {
+		return ctx;
+	}
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		ctx = this;
+		mSensorService = new SensorService(getCtx());
+		mServiceIntent = new Intent(getCtx(), mSensorService.getClass());
+		if (!isMyServiceRunning(mSensorService.getClass())) {
+			startService(mServiceIntent);
+		}
+		
 		view = findViewById(R.id.Test);
 		view.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -18,5 +34,24 @@ public class MainActivity extends AppCompatActivity {
 				Log.e(MainActivity.class.getSimpleName(), "Test");
 			}
 		});
+	}
+	
+	private boolean isMyServiceRunning(Class<?> serviceClass) {
+		ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+		for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+			if (serviceClass.getName().equals(service.service.getClassName())) {
+				Log.i ("isMyServiceRunning?", true+"");
+				return true;
+			}
+		}
+		Log.i ("isMyServiceRunning?", false+"");
+		return false;
+	}
+	
+	@Override
+	protected void onDestroy() {
+		// stopService(mServiceIntent);
+		Log.i("MAINACT", "onDestroy!");
+		super.onDestroy();
 	}
 }
